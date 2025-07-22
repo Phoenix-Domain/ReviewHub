f7c21bc13b1a0b3cfd1d9f0a3106bdd45ee91dd7
Batubo Victory <victorybatubo76@gmail.com>
2025-07-22 17:09:59 +0100

    Declare DOM variable

--- src/main.js
+++ src/main.js
@@ -1,2 +1,12 @@
-import './style.css'
+import './style.css';
+
+const name = document.querySelector('#name');
+const review = document.querySelector('#review');
+const image = document.querySelector('#image');
+const terribleRating = document.querySelector('#terrible');
+const poorRating = document.querySelector('#poor');
+const goodRating = document.querySelector('#good');
+const veryGoodRating = document.querySelector('#veryGood');
+const excellentRating = document.querySelector('#excellent');
+const testimonials = document.querySelector('#testimonials')
 
