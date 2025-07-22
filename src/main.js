a2c51c413c12357111a49595c0cdf8361509e16d
Batubo Victory <victorybatubo76@gmail.com>
2025-07-22 18:02:18 +0100

    Add submit button event listener

--- src/main.js
+++ src/main.js
@@ -1,13 +1,42 @@
 import './style.css';
 
+class MakeClient{
+  constructor(name,review,rating){
+    this.name = name;
+    this.review = review;
+    this.rating = rating;
+  }
+}
+
+
 const name = document.querySelector('#name');
 const review = document.querySelector('#review');
 const image = document.querySelector('#image');
-const terribleRating = document.querySelector('#terrible');
-const poorRating = document.querySelector('#poor');
-const goodRating = document.querySelector('#good');
-const veryGoodRating = document.querySelector('#veryGood');
-const excellentRating = document.querySelector('#excellent');
-const testimonials = document.querySelector('#testimonials')
+const ratings = document.querySelectorAll('.ratings');
+const testimonials = document.querySelector('#testimonials');
 const submitBtn = document.querySelector('#submitBtn');
 
+
+submitBtn.addEventListener('click', e => {
+  e.preventDefault();
+  const nameVal = name.value.trim();
+  const reviewVal = review.value.trim();
+  let ratingVal;
+  
+  ratings.forEach(rating => {
+    if(rating.id === "terrible" && rating.checked){
+      return ratingVal = 1;
+    } else if(rating.id === "poor" && rating.checked){
+      return ratingVal = 2;
+    } else if(rating.id === "good" && rating.checked){
+      return ratingVal = 3;
+    } else if(rating.id === "veryGood" && rating.checked){
+      return ratingVal = 4;
+    } else if(rating.id === "excellent" && rating.checked){
+      return ratingVal = 5;
+    }
+  })
+
+  let client = new MakeClient(nameVal,reviewVal,ratingVal)
+  console.log(client)
+})
